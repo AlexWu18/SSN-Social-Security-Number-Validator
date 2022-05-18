ac018a55f01bf925bfc34cd76ba2a586ae7031c1
AlexWu18 <65258331+AlexWu18@users.noreply.github.com>
2022-05-18 11:17:34 -0400

    Add project files.

---
+++ SSN (Social Security Number) Validator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+internal class program
+{
+    private static void Main(string[] args)
+    {
+        string PatternSSN = "^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$";
+        Regex regex = new Regex(PatternSSN);
+
+        bool Example1 = regex.IsMatch("736-21-5481");
+        Console.Write(Example1);
+        Console.ReadLine();
+
+    }
+
+}
